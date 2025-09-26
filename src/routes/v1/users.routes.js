dc33cbd1035e3328003e7912e0bbf0bf0a45616a
andres gonzalez <larserxd@gmail.com>
2025-09-26 16:10:50 -0500

    implementación de rutas GET (lista e individual).

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [
+    {
+        id: "1",
+        name:"Andres Felipe Gonzalez Noreña",
+        gender: "masculino",
+        age: "18",
+        password: "pipe123"
+    }
+]
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(u => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({ error: 'Usuario no encontrado'})
+    }
+    
+    res.status(200).json(users);
+});
+
+module.exports = router;
