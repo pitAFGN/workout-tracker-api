184310a71fd16bbb37ff38f7f86dddf1b74d991c
pitAFGN <andresfgn07xd@gmail.com>
2025-10-03 00:38:55 -0500

    implementación de rutas GET (lista e individual).

--- src/routes/v1/subscriptions.routes.js
+++ src/routes/v1/subscriptions.routes.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+let subscriptions = [
+    {
+        id: "1",
+        user_id: "1",
+        typeInscription: "mensual",
+        statusInscription: "activo"
+
+    }
+]
+
+router.get('/', (req, res) => {
+    res.status(200).json(subscriptions)
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const subscription = subscriptions.find(s => s.id === id);
+
+    if (!subscription) {
+        return res.status(404).json({ error: 'ejercicio no encontrado' })
+    }
+    res.status(200).json(subscriptions);
+});
+
+module.exports = router;
