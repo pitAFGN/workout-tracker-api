5ca7d8cff8ed7d1917a70d8ac954052a69204f3d
pitAFGN <andresfgn07xd@gmail.com>
2025-10-03 00:37:26 -0500

    scaffold de router y endpoints básicos.

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const usersRoutes = require('./users.routes')
 const exercisesRoutes = require('./exercises.routes')
+const subscriptionsRoutes = require('./subscriptions.routes')
 
 router.use('/users', usersRoutes);
 
 router.use('/exercises', exercisesRoutes);
+
+router.use('/subscriptions', subscriptionsRoutes);
 
 module.exports = router;
