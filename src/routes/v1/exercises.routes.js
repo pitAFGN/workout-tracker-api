addcc397f97809bf319c89033568819326b16ea2
pitAFGN <andresfgn07xd@gmail.com>
2025-10-02 22:55:32 -0500

    implementación de rutas GET (lista e individual).

--- src/routes/v1/exercises.routes.js
+++ src/routes/v1/exercises.routes.js
@@ -1,3 +1,28 @@
-const express = require ('express');
-const router  = express.Router();
+const express = require('express');
+const router = express.Router();
 
+let exercises = [
+    {
+        id: "1",
+        name: "prensa de pierna",
+        description: "entrenamiento de fuerza muy popular que se realiza con una máquina de pesas específica. Es ideal para desarrollar la masa muscular y la fuerza del tren inferior",
+        category: "fuerza",
+        muscleGroup: "piernas"
+    }
+]
+
+router.get('/', (req, res) => {
+    res.status(200).json(exercises)
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const exercise = exercises.find(e => e.id === id);
+
+    if (!exercise) {
+        return res.status(404).json({ error: 'ejercicio no encontrado'})
+    }
+res.status(200).json(exercises);
+});
+
+module.exports = router;
