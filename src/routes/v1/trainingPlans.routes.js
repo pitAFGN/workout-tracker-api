21046b91321dc600c6678069867007b99770702a
pitAFGN <andresfgn07xd@gmail.com>
2025-10-03 01:53:21 -0500

    eliminación con DELETE.

--- src/routes/v1/trainingPlans.routes.js
+++ src/routes/v1/trainingPlans.routes.js
@@ -85,4 +85,16 @@ router.patch('/:id', (req, res) => {
     res.status(200).json(trainingPlans[index]);
 });
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = trainingPlans.findIndex(e => e.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({ error: 'ejercicio no encontrado ome, espabile' });
+    }
+
+    const deletedPlans = trainingPlans.splice(index, 1);
+    res.status(200).json({ deleted: deletedPlans[0].id });    
+})
+
 module.exports = router;
